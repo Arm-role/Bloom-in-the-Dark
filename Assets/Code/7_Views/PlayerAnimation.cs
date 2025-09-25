8f6af9393a46effb1dc39ebd1e8bf1d30fff8f82
Armor <76558013+Arm-role@users.noreply.github.com>
2025-09-25 12:57:49 +0700

    addAnimationInPlayerScript

--- Assets/Code/7_Views/PlayerAnimation.cs
+++ Assets/Code/7_Views/PlayerAnimation.cs
@@ -4,19 +4,25 @@ public class PlayerAnimation : MonoBehaviour, IPlayerAnimationView
 {
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
     private static readonly int Vertical = Animator.StringToHash("Vertical");
-    private static readonly int Speed = Animator.StringToHash("Speed");
 
+    [SerializeField]
     private Animator _animator;
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
 
     public void SetMoveDirection(Vector2 moveDirection)
     {
         _animator.SetFloat(Horizontal, moveDirection.x);
         _animator.SetFloat(Vertical, moveDirection.y);
-        _animator.SetFloat(Speed, moveDirection.sqrMagnitude);
+
+        if (moveDirection.x > 0)
+        {
+            Vector3 scale = _animator.transform.localScale;
+            if (_animator.transform.localScale.x < 0) return;
+            _animator.transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+        }
+        else
+        {
+            Vector3 scale = _animator.transform.localScale;
+            _animator.transform.localScale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);
+        }
     }
 }
